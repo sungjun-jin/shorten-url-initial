d07a7d568001fa5277ce2979e838d04c805d208d
sungjun <jinsungjun94@gmail.com>
2024-09-29 17:16:05 +0900

    initial commit

---
+++ src/main/kotlin/me/sj/study/shortenurlinitial/ShortenUrlInitialApplication.kt
@@ -0,0 +1,11 @@
+package me.sj.study.shortenurlinitial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShortenUrlInitialApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShortenUrlInitialApplication>(*args)
+}
---
+++ src/test/kotlin/me/sj/study/shortenurlinitial/ShortenUrlInitialApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.sj.study.shortenurlinitial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShortenUrlInitialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
